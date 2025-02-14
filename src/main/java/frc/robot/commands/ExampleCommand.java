69794831f3d203581c8a8efd77e68e91d4d42c2b
Jay McNeely <j.t.mcneely06@gmail.com>
2025-02-14 12:12:27 -0600

    Updated Files for Linting.

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -8,16 +8,22 @@ import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends Command {
+public final class ExampleCommand extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
+  /**
+   * This is an example subsystem that should be used with this command
+   */
   private final ExampleSubsystem m_subsystem;
+  // This would need to bbe surpressed as it's not an actual error and it quite
+  // standard in some use cases such as this one
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
+  public ExampleCommand(final ExampleSubsystem subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
@@ -25,15 +31,18 @@ public class ExampleCommand extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(final boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
