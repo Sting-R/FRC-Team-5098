936053d78389693111a9c6d8c91f559c37271a5d
Henry Hare <dreadfall64@gmail.com>
2025-02-15 12:42:20 -0600

    Last merge didn't work (trying to open merge page between files) - H

--- 2024-2025/main-bot/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ 2024-2025/main-bot/src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,11 +1,11 @@
 package frc.robot.subsystems;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 //import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import java.util.function.BooleanSupplier;
 
 //import com.ctre.phoenix.Util;
 import com.ctre.phoenix6.hardware.TalonFX;
@@ -24,60 +24,63 @@ public class ElevatorSubsystem extends SubsystemBase {//makes elevator subsystem
     private final Encoder m_Encoder1;
     // private TitanQuadEncoder m_elevatorEncoder; 
     private double m_elevatorEncoder; // Placeholder
-    // Constants
-    private double motorElevatorSpeed;
+
+    private ElevatorLevels currentElevatorState;
+
 
 
 
   public ElevatorSubsystem(int elevatorLeftMotorId, int elevatorRightMotorId) {//this is da place where stuff is actually initialized
     m_elevatorEncoder = 0; // Placeholder
-    motorElevatorSpeed = ElevatorConstants.kMotorElevatorSpeed;
-
     m_elevatorLeftMotor = new TalonFX(elevatorLeftMotorId);
     m_elevatorRightMotor = new TalonFX(elevatorRightMotorId);
-    m_elevatorTopLimitSwitch = new DigitalInput(7);
-    m_elevatorBottomLimitSwitch = new DigitalInput(8);
+    m_elevatorTopLimitSwitch = new DigitalInput(1);
+    m_elevatorBottomLimitSwitch = new DigitalInput(2);
     m_Encoder1 = new Encoder(0, 1);
+    currentElevatorState = ElevatorLevels.Lowest;
   }
 
-public enum ElevatorPreset {
-    Level1,
+public enum ElevatorLevels {
+    Lowest, // bottom
+    Level1, // Levels of the reef
     Level2,
     Level3,
-    Level4
+    Level4,
+    Highest // top
 }
-  public Command raiseElevator() {
-    // Placeholder
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-            setMotorElevatorSpeed(0.1);
-        });
-  }
 
-    public Command lowerElevator() {
-        // Placeholder
-        return runOnce(
-            () -> {
-            /* one-time action goes here */ 
-                setMotorElevatorSpeed(-0.1);
-            });
-    }
+//The commands below that are commented are currently not being used and have been replaced. They are kept just cause
 
-    public void debuggingMethod() {
-        SmartDashboard.putNumber("Elevator Encoder", m_Encoder1.get());
-        SmartDashboard.putBoolean("Elevator Top Limit Switch", m_elevatorTopLimitSwitch.get());
-        SmartDashboard.putBoolean("Elevator Bottom Limit Switch", m_elevatorBottomLimitSwitch.get());
-    }
+  //public Command raiseElevator() {
+  //   Placeholder
+  //  return runOnce(
+  //      () -> {
+  //      /* one-time action goes here */
+  //          setMotorElevatorSpeed(0.1);
+  //      });
+  //}
+
+    // public Command lowerElevator() {
+    //     // Placeholder
+    //     return runOnce(
+    //         () -> {
+    //         /* one-time action goes here */ 
+    //             setMotorElevatorSpeed(-0.1);
+    //         });
+    // }
 
 
     public boolean isElevatorAtTop() {
         // Placeholder
-        if (m_Encoder1.get() >= 300  && m_elevatorTopLimitSwitch.get()) { // Placeholder. replace 0 in .get methods with the value that encoder would be at if at the top
+        if (m_Encoder1.get() < ElevatorConstants.kElevatorEncoderTopValue && !m_elevatorTopLimitSwitch.get()) {
+            return false;
+        }
+        else if(m_Encoder1.get() < ElevatorConstants.kElevatorEncoderTopValue && m_elevatorTopLimitSwitch.get()){
+            System.out.println("Error: Encoder And Top Limit Switch Mismatched");
             return true;
         }
-        else if(m_Encoder1.get() >= 300 || m_elevatorTopLimitSwitch.get()){
-            System.out.println("Error: Encoder And TOP Limit Switch Mismatched");
+        else if (m_Encoder1.get() >= ElevatorConstants.kElevatorEncoderTopValue && !m_elevatorTopLimitSwitch.get()) {
+            System.out.println("Error: Encoder And Top Limit Switch Mismatched");
             return true;
         }
         else {
@@ -87,11 +90,15 @@ public enum ElevatorPreset {
 
     public boolean isElevatorAtBottom() {
         // Placeholder
-        if (m_Encoder1.get() <= 0 && m_elevatorBottomLimitSwitch.get()) { // Placeholder. replace 0s in .get methods with what encoder would be at when at bottom
+        if (m_Encoder1.get() > ElevatorConstants.kElevatorEncoderBottomValue && !m_elevatorBottomLimitSwitch.get()) {
+            return false;
+        }
+        else if (m_Encoder1.get() > ElevatorConstants.kElevatorEncoderBottomValue && m_elevatorBottomLimitSwitch.get()){
+            System.out.println("Error: Encoder And Bottom Limit Switch Mismatched");
             return true;
         }
-        else if (m_Encoder1.get() <= 0 || m_elevatorBottomLimitSwitch.get()){
-            System.out.println("Error: Encoder And BOTTOM Limit Switch Mismatched");
+        else if (m_Encoder1.get() <= ElevatorConstants.kElevatorEncoderBottomValue && !m_elevatorBottomLimitSwitch.get()) {
+            System.out.println("Error: Encoder And Bottom Limit Switch Mismatched");
             return true;
         }
         else {
@@ -105,12 +112,11 @@ public enum ElevatorPreset {
         // BEN: If this method truly will be run once (as above), then the stop conditions will only be evaluated once.
         // We need to check the conditions continually somehow.
 
-        debuggingMethod();
-        if (voltage > 0 && !isElevatorAtTop()) {//if motor is moving and elevators at the top, make both move up
+        if (voltage > 0 && isElevatorAtTop()) {//if motor is moving and elevators at the top, make both move up
             m_elevatorLeftMotor.set(voltage);
             m_elevatorRightMotor.set(-voltage);
             Utility.printLn("Elevator is moving up");
-        } else if (voltage < 0 && !isElevatorAtBottom()) {//or if its at bottom make move down
+        } else if (voltage < 0 && isElevatorAtBottom()) {//or if its at bottom make move down
             m_elevatorLeftMotor.set(voltage);
             m_elevatorRightMotor.set(-voltage);
             Utility.printLn("Elevator is moving down");
@@ -122,19 +128,19 @@ public enum ElevatorPreset {
         
     }
 
-    public void moveElevatorToPreset(ElevatorPreset desiredPreset) {//tells elevator where to go based on certain presets using distgusting switch case statements
+    public void moveElevatorToPreset(ElevatorLevels desiredPreset) {//tells elevator where to go based on certain presets using distgusting switch case statements
         switch (desiredPreset) {
             case Level1:
-                moveElevatorToPosition(ElevatorConstants.ElevatorPreset.level1EncoderValue);
+                moveElevatorToPosition(ElevatorConstants.ElevatorPreset.level1EncoderValue, desiredPreset);
                 break;
             case Level2:
-                moveElevatorToPosition(ElevatorConstants.ElevatorPreset.level2EncoderValue);
+                moveElevatorToPosition(ElevatorConstants.ElevatorPreset.level2EncoderValue, desiredPreset);
                 break;
             case Level3:
-                moveElevatorToPosition(ElevatorConstants.ElevatorPreset.level3EncoderValue);
+                moveElevatorToPosition(ElevatorConstants.ElevatorPreset.level3EncoderValue, desiredPreset);
                 break;
             case Level4:
-                moveElevatorToPosition(ElevatorConstants.ElevatorPreset.level4EncoderValue);
+                moveElevatorToPosition(ElevatorConstants.ElevatorPreset.level4EncoderValue, desiredPreset);
                 break;
         }
     }
@@ -144,41 +150,47 @@ public enum ElevatorPreset {
      * fun fact: 6!! is 6 * 4 * 2 while 7!! is 7 * 5 * 3 * 1. Kind of like a selective factorial. Yeah idk why I said that. Moving on
      * @param position
      */
-    public void moveElevatorToPosition (double position) {
+    public void moveElevatorToPosition (double position, ElevatorLevels desiredPreset) {//moves elevator to a certain position based on the encoder value
         // WILL HAVE TO REVERSE ONE MOTOR DEPENDING ON ORIENTATION!!!!
-        debuggingMethod();
-        if (m_Encoder1.get() <= (position + 5) && !isElevatorAtBottom()) { // checking to see if the motor wants to move down and makes sure the elevator isn't at the bottom
-            m_elevatorLeftMotor.set(-motorElevatorSpeed);
-            m_elevatorRightMotor.set(motorElevatorSpeed);
+        if (m_elevatorEncoder <= position && !isElevatorAtBottom()) { // checking to see if the motor wants to move down and makes sure the elevator isn't at the bottom
+            m_elevatorLeftMotor.set(-ElevatorConstants.kMotorElevatorSpeed);
+            m_elevatorRightMotor.set(ElevatorConstants.kMotorElevatorSpeed);
             Utility.printLn("Elevator is moving down");
-        } else if (m_Encoder1.get() >= (position -5) && !isElevatorAtTop()) { // checking to see if the motor wants to move up and makes sure the elevator isn't at the top
-            m_elevatorLeftMotor.set(motorElevatorSpeed);
-            m_elevatorRightMotor.set(-motorElevatorSpeed);
+        } else if (m_elevatorEncoder >= position && !isElevatorAtTop()) { // checking to see if the motor wants to move up and makes sure the elevator isn't at the top
+            m_elevatorLeftMotor.set(ElevatorConstants.kMotorElevatorSpeed);
+            m_elevatorRightMotor.set(-ElevatorConstants.kMotorElevatorSpeed);
             Utility.printLn("Elevator is moving up");
         } else {
             m_elevatorLeftMotor.set(0); 
             m_elevatorRightMotor.set(0);
             Utility.printLn("Elevator is at the top, bottom, desired position, or error occurred and therefore the motors have been stopped");
+            currentElevatorState = desiredPreset;
         }
     }
 
-    
+    public ElevatorLevels getElevatorState() {
+        return currentElevatorState;
+    }
 
-    public Command commandVoltage(double voltage) {//just exists because just felt like it
+    public BooleanSupplier isElevatorAtDesiredState(ElevatorLevels desiredState) {
+        return () -> currentElevatorState == desiredState;
+    }
+
+    public Command commandVoltage(double leftTrigger, double rightTrigger) {//just exists because just felt like it
         return runOnce(
             () -> {
-                setMotorElevatorSpeed(voltage); 
+                setMotorElevatorSpeed(rightTrigger - leftTrigger); 
             }
         );
     }
 
-    // public Command commandMoveToPreset(ElevatorPreset desiredPreset) {//does the function at line 134 but with more... you know...
-    //     return runOnce(
-    //         () -> {
-    //             moveElevatorToPreset(desiredPreset); 
-    //         }
-    //     );
-    // }
+    public Command commandMoveToPreset(ElevatorLevels desiredPreset) {//The first function in a very tall dependancy tree
+        return runOnce(
+            () -> {
+                moveElevatorToPreset(desiredPreset); 
+            }
+        );
+    }
     
 }
 //a comment because why not :)
